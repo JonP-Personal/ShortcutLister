a075a8182577169619363de39d7bbec02138aad6
Jon Perrine <jon.perrine@gmail.com>
2017-04-21 18:54:48 -0700

    Initial checkin. Partially working.

---
+++ ShortcutLister/App.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+namespace ShortcutLister
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon NotifyIcon = null;
+
+        public App()
+        {
+            ShowNotificationIcon();            
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            RemoveNotificationIcon();
+        }
+
+        /// <summary>
+        /// Shows the notification icon in the system tray
+        /// </summary>
+        private void ShowNotificationIcon()
+        {
+            Stream iconStream = Application.GetResourceStream(new Uri("pack://application:,,,/Main2.ico")).Stream;
+
+            NotifyIcon = new System.Windows.Forms.NotifyIcon();
+            NotifyIcon.Icon = new System.Drawing.Icon(iconStream);
+            NotifyIcon.Visible = true;
+            NotifyIcon.ContextMenuStrip = CreateContextMenu();
+
+            iconStream.Dispose();
+
+            return;
+        }
+
+        /// <summary>
+        /// Removes the notification icon from the system tray
+        /// </summary>
+        private void RemoveNotificationIcon()
+        {
+            if (NotifyIcon != null)
+                NotifyIcon.Visible = false;
+            return;
+        }
+
+        /// <summary>
+        /// Creates the context menu, which is a list of all Shortcuts in the "Documents\ShortcutLister" folder
+        /// </summary>
+        /// <returns>The context menu to use</returns>
+        private System.Windows.Forms.ContextMenuStrip CreateContextMenu()
+        {
+            System.Windows.Forms.ContextMenuStrip contextMenu = null;
+            System.Windows.Forms.ToolStripMenuItem menuItem = null;
+            ShortcutHelper helper = new ShortcutHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ShortcutLister"));
+            List<ShortcutItem> listShortcuts = null;
+            System.Drawing.Icon icon = null;
+
+            contextMenu = new System.Windows.Forms.ContextMenuStrip();
+            listShortcuts = helper.GetShortcutItems();
+
+            foreach (ShortcutItem item in listShortcuts)
+            {
+                icon = System.Drawing.Icon.ExtractAssociatedIcon(item.TargetFileName);                
+
+                menuItem = new System.Windows.Forms.ToolStripMenuItem();
+                menuItem.Text = item.DisplayName;                
+                menuItem.Image = icon.ToBitmap();
+                menuItem.Click += MenuItem_Click;
+                menuItem.Tag = item;
+
+                icon.Dispose();                
+
+                contextMenu.Items.Add(menuItem);
+            }
+                        
+            return contextMenu;
+        }
+
+        /// <summary>
+        /// Called when menu item clicked. This launches the shortcut that was selected.
+        /// </summary>
+        /// <param name="sender">The clicked on ToolStripMenuItem object</param>
+        /// <param name="e">Event args</param>
+        private void MenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ToolStripMenuItem itemSelected = (System.Windows.Forms.ToolStripMenuItem)sender;
+            ProcessStartInfo process = new ProcessStartInfo();
+            ShortcutItem item = (ShortcutItem)itemSelected.Tag;
+
+            process.Arguments = item.Arguments;
+            process.WorkingDirectory = item.WorkingDirectory;
+            process.FileName = item.TargetFileName;
+
+            if (item.ShowCommand == ShortcutItem.SHOWCOMMAND_RESTORE)
+                process.WindowStyle = ProcessWindowStyle.Normal;
+            else if (item.ShowCommand == ShortcutItem.SHOWCOMMAND_MINIMIZE)
+                process.WindowStyle = ProcessWindowStyle.Minimized;
+            else if (item.ShowCommand == ShortcutItem.SHOWCOMMAND_MAXIMIZE)
+                process.WindowStyle = ProcessWindowStyle.Maximized;
+
+            Process.Start(process);
+
+            return;
+        }
+    }
+}
---
+++ ShortcutLister/ShortcutHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ShortcutLister
+{
+    class ShortcutItem
+    {
+        public static readonly int SHOWCOMMAND_RESTORE = 1;
+        public static readonly int SHOWCOMMAND_MINIMIZE = 2;
+        public static readonly int SHOWCOMMAND_MAXIMIZE = 3;
+
+        public String DisplayName = null;
+        public String ShortcutFileName = null;
+        public String TargetFileName = null;
+        public String IconFile = null;
+
+        public String Arguments = null;
+        public String WorkingDirectory = null;
+        public int ShowCommand = 0;
+    }
+
+    class ShortcutHelper
+    {
+        private String Folder = null;
+
+        public ShortcutHelper(String sFolder)
+        {
+            Folder = sFolder;
+        }
+
+        public List<ShortcutItem> GetShortcutItems()
+        {
+            List<ShortcutItem> list = new List<ShortcutItem>();
+            string[] sFiles = null;
+            ShortcutItem item = null;
+            Shell32.Shell shell = new Shell32.Shell();
+            Shell32.Folder folder = null;
+            Shell32.FolderItem folderItem = null;
+            Shell32.ShellLinkObject shellLink = null;
+            string sPathOnly = null;
+            string sFileOnly = null;           
+
+            sFiles = Directory.GetFiles(Folder, "*.lnk");
+            foreach (string sFile in sFiles)
+            {
+                item = new ShortcutItem();
+                item.ShortcutFileName = sFile;
+
+                // Get properties from the shortcut file
+                sPathOnly = Path.GetDirectoryName(sFile);
+                sFileOnly = Path.GetFileName(sFile);
+                folder = shell.NameSpace(sPathOnly);
+                folderItem = folder.ParseName(sFileOnly);
+                if (folderItem != null)
+                {
+                    shellLink = folderItem.GetLink;
+
+                    item.TargetFileName = shellLink.Path;
+                    item.DisplayName = folderItem.Name;
+                    item.Arguments = shellLink.Arguments;
+                    item.WorkingDirectory = shellLink.WorkingDirectory;
+                    item.ShowCommand = shellLink.ShowCommand;
+
+                    shellLink.GetIconLocation(out item.IconFile);
+
+                    list.Add(item);
+                }
+            }
+
+            return list;
+        }
+
+    }
+}
